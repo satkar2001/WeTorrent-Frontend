bcddb0ea4ca09026b8055724110bdc84210e7d33
Satkar Acharya <81009537+satkar2001@users.noreply.github.com>
2022-12-28 12:14:44 +0530

    Update Torrents.js

--- src/components/Torrents.js
+++ src/components/Torrents.js
@@ -4,7 +4,7 @@ import Form from "react-bootstrap/Form";
 import { useLocation } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 
-const mylocaldomain="https://wetorrent.onrender.com/";
+const mylocaldomain="https://wetorrent.onrender.com";
 
 const Torrents = () => {
     const [torrents,setTorrents] = useState([]);
